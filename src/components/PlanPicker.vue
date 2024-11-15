d640dfed59b355a91da6a81fcf20deb64ed5ed9c
liz66-g <litib279@gmail.com>
2024-11-15 17:02:25 +0000

    act15-11-24

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,13 +1,20 @@
  <script>
 import { ref } from 'vue';
 import planItem from './PlanPickerItem.vue';
+//referencia reactiva para almacenar el plan seleccionado 
+const selectedPlan = ref(null);
 const plans = ref(['El cafetero','El chelero','El intelectual']);
+const selectPlan=(name) =>{
+  //Almacenar 
+  selectPlan.value=name;
+};
 </script>
 
 <template>
 <div class="plans">
       <planItem v-for="plan in plans" :key="plan" :name="plan" 
-      @select="(name)=>{console.log(name)}"/>
+      @select="(name)=>
+      {console.log(name)}"/>
     </div> 
 </template>
 <div>
